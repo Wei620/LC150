a1f77bee31167b1f74ef29a54e31764a18ed5d78
Wei620 <weizhao620@gmail.com>
2016-02-13 10:59:47 -0600

    No need to reset p in the inner loop.
    Compare c and prev instead.

--- Medium/InsertionSortList.java
+++ Medium/InsertionSortList.java
@@ -16,14 +16,20 @@ class InsertionSortList {
         if (head == null || head.next == null) return head;
         ListNode pre = new ListNode(0);
         pre.next = head;
+		
+		/*
+		 * pre - check the insertion position from the begining.
+		 * p - current element
+		 * prev - the element before the current one
+		 * c - the p may insert after c.
+		 */
         
-        for (ListNode p = head.next, prev = head; p != null; prev = p, p = p.next) {
-            for (ListNode c = pre; c.next != p; c = c.next) {
+        for (ListNode p = head.next, prev = head; p != null; prev = p, p = p.next) {// prev -> p
+            for (ListNode c = pre; c != prev; c = c.next) {// p is right after prev already.       
                 if (c.next.val > p.val) { 
                     prev.next = p.next; // skip p
                     p.next = c.next; // insert between cur and cur.next
                     c.next = p;
-                    p = prev; // p is inserted to somewhere in the front, reset
                     break;
                 }
             }
