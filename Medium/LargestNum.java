97cfb82a8c0026f8045a8e59c8425c525f4a8214
Wei620 <weizhao620@gmail.com>
2016-02-13 17:21:51 -0600

    Added comments.

--- Medium/LargestNum.java
+++ Medium/LargestNum.java
@@ -35,7 +35,11 @@ class LargestNum {
             }
         };
         Arrays.sort(str, comp); // acsending order regarding to comp, which prefers the -1 output of comp.
-        //str[0] pair with any str[i], str[i]+str[0] is smaller than str[0]+str[i]. It implies 
+        /*
+         * str[0] is the smallest element in order that defined by comp.
+         * It implies that  for any i, comp.compare(str[0], str[i]) ==  -1,
+         * So str[i] + str[0] is less than str[0] + str[i].
+         */
         if (str[0].equals("0")) return "0"; // deal with 0
         for (String s : str) res.append(s);
         return res.toString();
