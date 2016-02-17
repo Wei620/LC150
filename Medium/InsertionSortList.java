fce7cb3398d70fa36d7a453cdd8a5fb02379bbf6
Wei620 <weizhao620@gmail.com>
2016-02-16 22:06:05 -0600

    Update implementation.

--- Medium/InsertionSortList.java
+++ Medium/InsertionSortList.java
@@ -14,27 +14,27 @@ class InsertionSortList {
      */
     public static ListNode insertionSortList(ListNode head) {
         if (head == null || head.next == null) return head;
-        ListNode pre = new ListNode(0);
-        pre.next = head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
 		
 		/*
-		 * pre - check the insertion position from the begining.
+		 * dummy - check the insertion position from the begining.
 		 * p - current element
-		 * prev - the element before the current one
+		 * pre - the element before the current one
 		 * c - the p may insert after c.
 		 */
         
-        for (ListNode p = head.next, prev = head; p != null; prev = p, p = p.next) {// prev -> p
-            for (ListNode c = pre; c != prev; c = c.next) {// p is right after prev already.       
-                if (c.next.val > p.val) { 
-                    prev.next = p.next; // skip p
-                    p.next = c.next; // insert between cur and cur.next
+        for (ListNode p = head.next, pre = head; p != null; pre = p, p = p.next) {// pre -> p
+            for (ListNode c = dummy; c != pre; c = c.next) {//c!=pre because p is right after pre already.       
+                if (c.next.val > p.val) {  // ascending sort
+                    pre.next = p.next; // skip p
+                    p.next = c.next; // insert right after c
                     c.next = p;
                     break;
                 }
             }
         }
-        return pre.next;
+        return dummy.next;
     }
     
     public static class ListNode {
